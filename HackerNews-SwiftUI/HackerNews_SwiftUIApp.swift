7220c8e37deffc901e49c27c57b36f279764725c
Raihan Firzie <alraihan77@gmail.com>
2024-06-10 15:12:43 +0700

    Initial Commit

---
+++ HackerNews-SwiftUI/HackerNews_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNews_SwiftUIApp.swift
+//  HackerNews-SwiftUI
+//
+//  Created by Raihan on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNews_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
